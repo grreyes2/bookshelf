36058b16c7922d2dba76009735c625eaeb4758c8
Gensel <genselreyes@gmail.com>
2020-08-03 19:40:27 -0500

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :first_name, :last_name, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :favorites
+  has_many :books, through: :assignments
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  get 'favorites/update'
+  resources :books
+  devise_for :users
+  root to: 'books#index'
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
